e026aa6261b77ca1de87fb11b4d377420754774c
haopeng <haopeng@bilibili.com>
2021-09-18 15:57:04 +0800

    init

---
+++ MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App6
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var root = Window.Current.Content as FrameworkElement;
+            root.RequestedTheme = root.RequestedTheme == ElementTheme.Dark ? ElementTheme.Light : ElementTheme.Dark; ;
+        }
+    }
+}
---
+++ RichText.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace App6
+{
+    public sealed class RichText : Control
+    {
+        public RichText()
+        {
+            this.DefaultStyleKey = typeof(RichText);
+        }
+
+        private RichTextBlock _richTextBlock;
+
+        protected override void OnApplyTemplate()
+        {
+
+            if (_richTextBlock == null)
+            {
+                _richTextBlock = GetTemplateChild("RichTextBlock") as RichTextBlock;
+            }
+
+            var _curBlock = new Paragraph();
+            _richTextBlock.Blocks.Add(_curBlock);
+
+            var inline = new Hyperlink()
+            {
+                UnderlineStyle = UnderlineStyle.None
+            };
+            inline.Inlines.Add(new Run() { Text = "@one" });
+
+            _curBlock.Inlines.Add(inline);
+
+            base.OnApplyTemplate();
+        }
+    }
+}
